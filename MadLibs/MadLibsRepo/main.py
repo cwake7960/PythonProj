3830ede03f5e73f49605adefb6727885fe597cd2
cwake7960 <49754203+cwake7960@users.noreply.github.com>
2021-09-22 14:12:48 -0500

    init

---
+++ MadLibs/MadLibsRepo/main.py
@@ -0,0 +1,12 @@
+#youtouber = 'Poopie Nugget'
+
+#print("suscribe to {}".format(youtouber))
+#print(f"suscribe to {youtouber}")
+
+mans_name = input("mans name:")
+body_part = input("body part:")
+farm_anmial = input("farm anmal:")
+
+madlib = f"there once was {mans_name} that once stuck his {body_part} in a {farm_anmial}."
+
+print(madlib)
