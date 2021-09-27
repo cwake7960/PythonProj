33efa0fad223b123565116b5eec113697325d8b3
cwake7960 <49754203+cwake7960@users.noreply.github.com>
2021-09-27 14:11:38 -0500

    9272021

--- TicTacToe/player.py
+++ TicTacToe/player.py
@@ -13,12 +13,25 @@ class RandomComputerPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        square = random.choice(game.available_moves())
+        return square
 
 class HumanPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
-        
+        valad_square = False
+        val = None
+        while not valad_square:
+            square = input(self.letter + '\'s turn. Input move (0-9):')
+
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valad_square = True
+            except ValueError:
+                print('Invalad square. Try agian.')
+
+        return val
